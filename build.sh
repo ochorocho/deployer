57539c7112994abe88bceac20aead93936582844
ochorocho <jochen.roth@b13.com>
2023-03-03 00:56:46 +0100

    [TASK] Rework build workflow

--- build.sh
+++ build.sh
@@ -1,4 +1,47 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-docker build --progress plain --no-cache --pull . -f Dockerfile -t ochorocho/deployer:latest && docker run --rm -it -v `pwd`/test.sh:/tmp/test.sh --entrypoint "ash" ochorocho/deployer:latest /tmp/test.sh
-docker images | grep docker-deployer
+declare -A IMAGE_OPTIONS
+IMAGE_OPTIONS[alpine_version]="3.17"
+IMAGE_OPTIONS[php_version]="81"
+IMAGE_OPTIONS[deployer_version]="7.2"
+
+help() {
+    echo "Available options:"
+    echo "  * i - alpine image version, e.g. 3.17"
+    echo "  * p - php version given by the alpine image e.g. 81"
+    echo "  * d - deployer version e.g. 7.2"
+}
+
+while getopts ":i:p:d:h" opt; do
+  case $opt in
+  h)
+    help
+    exit 1
+    ;;
+  i)
+    IMAGE_OPTIONS[alpine_version]="${OPTARG}"
+    ;;
+  p)
+    IMAGE_OPTIONS[php_version]="${OPTARG}"
+    ;;
+  d)
+    IMAGE_OPTIONS[deployer_version]="${OPTARG}"
+    ;;
+  *)
+    echo "Invalid option: -$OPTARG"
+    help
+    exit 1
+    ;;
+  esac
+done
+
+echo "Image information:"
+build_args=""
+for i in "${!IMAGE_OPTIONS[@]}"
+do
+  echo "  ${i}: ${IMAGE_OPTIONS[$i]}"
+  build_args+="--build-arg ${i}=\"${IMAGE_OPTIONS[$i]}\" "
+done
+
+docker build --progress plain --no-cache --pull . -f Dockerfile -t "ochorocho/deployer:${IMAGE_OPTIONS[deployer_version]}" && \
+docker run --rm -it -v "$(pwd)/test.sh:/tmp/test.sh" --entrypoint "ash" "ochorocho/deployer:${IMAGE_OPTIONS[deployer_version]}" /tmp/test.sh
